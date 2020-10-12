a72f7a24f2b22f793cb2c8893d9c64e756985861
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-10-12 11:52:21 -0300

    Increased the limit of fn execution

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ bag.forEach(w => {
       console.log(w, x, y, o, hasCollision);
     }
 
-    if (hasCollision && callStackCounter <= 6000) {
+    if (hasCollision && callStackCounter <= 10000) {
       callStackCounter += 1;
       return prepareCells();
     }
